b61e99de58373418ea938a68a06823f2ef433ab2
JenkyTran <tranvanluc.work@gmail.com>
2024-11-18 20:39:10 +0700

    [Update] Solve math

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import math
 from prompt import get_response
+from prompt import get_solve
+
 
 # Hàm tính tổ hợp
 def combination(n, k):
@@ -15,10 +17,19 @@ st.sidebar.markdown("<h1 style='text-align: center; color: #FF6347;'></h1>", uns
 
 
 st.sidebar.markdown("<h4 style='text-align: center; color: #4682B4;'>Chọn một tính năng:</h4>", unsafe_allow_html=True)
-
+st.sidebar.markdown("1. Mô tả bài toán hoán vị")
+st.sidebar.markdown("2. Mô tả bài toán tổ hợp")
+st.sidebar.markdown("3. Mô tả bài toán chỉnh hợp")
+st.sidebar.markdown("4. Hướng dẫn giải bài toán")
+st.sidebar.markdown("")
+st.sidebar.markdown("")
 # Lưu trạng thái của tính năng đã chọn vào session_state
 if "type_math" not in st.session_state:
     st.session_state.type_math = ""
+
+
+
+
 
 # Sử dụng các button lớn hơn cho các tính năng
 col1, col2, col3 = st.sidebar.columns([1, 1, 1])
@@ -32,28 +43,52 @@ with col3:
     if st.button("📐 Chỉnh hợp", key="chinh_hop"):
         st.session_state.type_math = "ch"
 
-# Giao diện chính với hai cột
-col_main, col_chat, col_pad = st.columns([1, 5, 1])
+# Nút hướng dẫn được căn giữa
+with st.sidebar:
+    st.write("---")
+    empty_col1, center_col, empty_col3 = st.columns([1, 2, 1])
+    with center_col:
+        if st.button("❓ Hướng dẫn giải bài", key="huong_dan"):
+            st.session_state.type_math = "solve"
 
-# Nội dung cột bên phải (nơi hiển thị chat và giới thiệu hệ thống)
+
+col_main, col_chat, col_pad = st.columns([1, 8, 1])
+
+members = [
+    {"name": "Huy", "class": "10A1", "school": "THCS", "image": "image/man-avatar-icon-free-vector.jpg"},
+    {"name": "Quân", "class": "11B2", "school": "THCS", "image": "image/man-avatar-icon-free-vector.jpg"},
+    {"name": "Khánh", "class": "12C3", "school": "THCS", "image": "image/man-avatar-icon-free-vector.jpg"},
+]
+
 with col_chat:
     st.markdown("<h1 style='text-align: center; color: #4682B4;'>Chào mừng đến với Easy Math!</h1>",
                 unsafe_allow_html=True)
     # Nút giới thiệu hệ thống và thông điệp chào mừng
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
+    st.markdown("")
+    if st.button(" Giới Thiệu"):
+        st.write("""
+            Easy Math là một công cụ học toán trực tuyến sử dụng công nghệ AI, giúp giải thích các công thức, bài toán phức tạp như hoán vị, tổ hợp và chỉnh hợp thành những mô tả dễ hiểu. Được phát triển bởi nhóm các bạn học sinh bằng công nghệ GenAI và ngôn ngữ lập trình Python, Easy Math giúp học sinh và giáo viên tiếp cận toán học một cách dễ dàng hơn, đồng thời tiết kiệm thời gian và nâng cao hiệu quả học tập. Truy cập Easy Math để trải nghiệm cách học toán mới mẻ, đơn giản và hiệu quả!
+            """)
+        # Tạo 3 cột
+        col1, col2, col3 = st.columns(3)
 
-    if st.button("📢 Giới Thiệu Hệ Thống"):
-        st.write(
-            "Chào các bạn! Đây là hệ thống giúp bạn học các kiến thức toán về hoán vị, tổ hợp và chỉnh hợp một cách dễ dàng thông qua sự chuyển đổi các phép toán khô khan thành các mô tả gần gũi, dễ hiểu.")
-
-    # Nhập số lượng phần tử
-    n = st.number_input("Nhập số lượng phần tử (n)", min_value=1, step=1)
-    if st.session_state.type_math != "hv":
-        # Nhập số phần tử chọn
-        k = st.number_input("Nhập số phần tử chọn (k)", min_value=1, max_value=n, step=1)
-    else:
+        # Đặt mỗi thành viên vào một cột
+        for col, member in zip([col1, col2, col3], members):
+            with col:
+                st.image(member["image"], use_column_width=True)
+                st.write(f"### {member['name']}")
+                st.write(f"**Lớp:** {member['class']}")
+                st.write(f"**Trường:** {member['school']}")
+    st.write("---")
+    n = 1000
+    if st.session_state.type_math == "hv" or st.session_state.type_math == "th" or st.session_state.type_math == "ch":
+        n = st.number_input("Nhập số lượng phần tử (n)", min_value=1, step=1)
+    if st.session_state.type_math == "hv":
         k = None
+    elif st.session_state.type_math == "ch" or st.session_state.type_math == "th":
+            k = st.number_input("Nhập số phần tử chọn (k)", min_value=1, max_value=n, step=1)
 
     answer = ""
     if st.session_state.type_math == "th":
@@ -66,23 +101,30 @@ with col_chat:
         st.write(f"Tính A({n}, {k})")
         answer = permutation(n, k)
 
-    # Nút mô tả để lấy kết quả mô tả từ hệ thống ChatGoogleGenerativeAI
-    # Khi nhấn nút "Mô tả"
-    gen_des = st.button("Mô tả", key="mota")
-    if gen_des:
-        response = get_response(st.session_state.type_math, n, k)
-        st.session_state.response = response  # Lưu response vào session_state
-        st.write(response)
+    if st.session_state.type_math == "hv" or st.session_state.type_math == "th" or st.session_state.type_math == "ch":
+        gen_des = st.button("Mô tả", key="mota")
+        if gen_des:
+            response = get_response(st.session_state.type_math, n, k)
+            st.session_state.response = response  # Lưu response vào session_state
+            st.write(response)
 
     # # Hiển thị nút "Đáp án" chỉ khi gen_des đã được nhấn
-    # if 'response' in st.session_state:
-    #     key_ans = st.button("Đáp án")
-    #     if key_ans:
-    #         st.write(st.session_state.response)  # Hiển thị response đã lưu
-    #         st.write(answer)  # Hiển thị đáp án
+    if 'response' in st.session_state:
+        key_ans = st.button("Đáp án")
+        if key_ans:
+            st.write(st.session_state.response)  # Hiển thị response đã lưu
+            st.write("Đáp án: ", answer)  # Hiển thị đáp án
+    if st.session_state.type_math == "solve":
+        # Hộp nhập liệu cho người dùng
+        user_message = st.text_input("Nhập đề bài: ", key="user_input", placeholder="Nhập nội dung tại đây...")
+        if user_message:
+            response = get_solve(user_message)
+            st.session_state.response = response  # Lưu response vào session_state
+            st.write(response)
 
 # Hiển thị nội dung hướng dẫn trong sidebar dựa trên tính năng đã chọn
 if st.session_state.type_math == "hv":
+    st.sidebar.write("---")
     st.sidebar.write("# Bạn đã chọn tính năng Hoán vị 🎲")
     st.sidebar.markdown("### Hoán Vị")
     st.sidebar.markdown("- Hoán vị là cách sắp xếp các phần tử.")
@@ -91,8 +133,9 @@ if st.session_state.type_math == "hv":
         P(n) = n!
     ''')
     result = math.factorial(n)
-    st.sidebar.write(f"Kết quả: Hoán vị của {n} phần tử là: {result}")
+    # st.sidebar.write(f"Kết quả: Hoán vị của {n} phần tử là: {result}")
 elif st.session_state.type_math == "th":
+    st.sidebar.write("---")
     st.sidebar.write("# Bạn đã chọn tính năng Tổ hợp 🔢")
     st.sidebar.markdown("### Tổ Hợp")
     st.sidebar.markdown("- Tổ hợp là cách chọn các phần tử mà không quan tâm đến thứ tự.")
@@ -101,8 +144,9 @@ elif st.session_state.type_math == "th":
         C(n, k) = \binom{n}{k} = \frac{n!}{k!(n-k)!}
     ''')
     result = combination(n, k)
-    st.sidebar.write(f"Kết quả: Tổ hợp của {n} phần tử chọn {k} là: {result}")
+    # st.sidebar.write(f"Kết quả: Tổ hợp của {n} phần tử chọn {k} là: {result}")
 elif st.session_state.type_math == "ch":
+    st.sidebar.write("---")
     st.sidebar.write("# Bạn đã chọn tính năng Chỉnh hợp 📐")
     st.sidebar.markdown("### Chỉnh Hợp")
     st.sidebar.markdown("- Chỉnh hợp là cách chọn các phần tử mà có quan tâm đến thứ tự.")
@@ -111,4 +155,4 @@ elif st.session_state.type_math == "ch":
         A(n, k) = \frac{n!}{(n-k)!}
     ''')
     result = permutation(n, k)
-    st.sidebar.write(f"Kết quả: Chỉnh hợp của {n} phần tử chọn {k} là: {result}")
+    # st.sidebar.write(f"Kết quả: Chỉnh hợp của {n} phần tử chọn {k} là: {result}")
--- prompt.py
+++ prompt.py
@@ -8,6 +8,8 @@ llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash', temperature=0.9)
 
 def create_prompt(context):
     return prompt_template.format(context=context)
+def create_prompt_solve(context):
+    return prompt_template_solve.format(context=context)
 
 prompt_template = """
     Bạn là một chuyên gia trong lĩnh vực hoán vị, tổ hợp, chỉnh hợp và bạn biết các kiến thức sau:
@@ -31,6 +33,33 @@ prompt_template = """
     Mô tả: Một tổ có 5 bạn học sinh. Có bao nhiêu cách để chọn ra 3 bạn học sinh đi trực nhật
     """
 
+prompt_template_solve = """
+    Bạn là một chuyên gia trong lĩnh vực hoán vị, tổ hợp, chỉnh hợp và bạn biết các kiến thức sau:
+    -Hoán vị là cách sắp xếp các phần tử.
+    -Tổ hợp là cách chọn các phần tử mà không quan tâm đến thứ tự.
+    -Chỉnh hợp là cách chọn các phần tử mà có quan tâm đến thứ tự.
+    ####
+    Đề bài: {context}
+    ####
+    Bạn hãy đưa ra hướng dẫn giải bài toán ở trên theo mẫu yêu cầu sau:
+    1/ Xác định đây là bài toán hoán vị hay tổ hợp hay chỉnh hợp trong 1 câu.
+    2/ Hướng dẫn các bước để giải bài toán (lưu ý không đưa đáp án cụ thể, không sử dụng công thức ở dạng latex, dùng ở dạng A(n, k); P(n); C(n, k)). Nếu có thể thì hướng dẫn giải bằng nhiều cách.
+    ####
+    Mẫu câu hỏi và câu trả lời tham khảo:
+    a) Sắp xếp năm bạn học sinh An, Bình, Chi, Dũng, Lệ vào một chiếc ghế dài có 5 chỗ ngồi. Số cách sắp xếp sao cho bạn Chi luôn ngồi chính giữa là
+    Giải: 
+    1/ Đây là bài toán hoán vị 
+    2/ Xếp bạn Chi ngồi giữa có 1 cách. Số cách xếp 4 bạn sinh An, Bình, Dũng, Lệ vào 4 chỗ còn lại là một hoán vị của 4 phần tử
+    b) Có bao nhiêu cách xếp khác nhau cho 4 người ngồi vào 6 chỗ trên một bàn dài?
+    Giải: 
+    1/ Đây là bài toán chỉnh hợp 
+    2/ Số cách xếp khác nhau cho 4 người ngồi vào 6 chỗ là một bài toán có quan tâm đến thứ tự (vị trí) nên đáp án sẽ là chỉnh hợp chập 4 của 6 phần tử
+    c) Một lớp học có 40 học sinh gồm 25 nam và 15 nữ. Chọn 3 học sinh để tham gia vệ sinh công cộng toàn trường, hỏi có bao nhiêu cách chọn như trên?
+    Giải: 
+    1/ Đây là bài toán tổ hợp 
+    2/ Nhóm học sinh 3 người được chọn (không phân biệt nam, nữ - công việc) là một tổ hợp chập 3 của 40 (học sinh).
+    """
+
 def get_response(type_math, n, k):
     if type_math == 'th':
         context = 'tổ hợp C(' + str(n) + ', ' + str(k) + ')'
@@ -42,4 +71,8 @@ def get_response(type_math, n, k):
     response = llm.invoke(prompt).content
     return response
 
+def get_solve(context):
+    prompt = create_prompt_solve(context)
+    response = llm.invoke(prompt).content
+    return response
 # print(get_response('th', 3, 2))
